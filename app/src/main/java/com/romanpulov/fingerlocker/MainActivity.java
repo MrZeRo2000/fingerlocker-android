1f0e4e862e9cc6bf8c71092cb2c025ffd2f28558
MrZeRo2000 <MrZeRo2000@mail.ru>
2021-01-07 17:23:04 +0200

    first commit

---
+++ app/src/main/java/com/romanpulov/fingerlocker/MainActivity.java
@@ -0,0 +1,14 @@
+package com.romanpulov.fingerlocker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        finish();
+    }
+}
