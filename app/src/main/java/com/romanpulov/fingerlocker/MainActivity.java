b8ad11f52a5b635e3f5d6e890cf3a944b0928ec5
MrZeRo2000 <MrZeRo2000@mail.ru>
2021-01-11 17:49:50 +0200

    implemented admin registration and lock

--- app/src/main/java/com/romanpulov/fingerlocker/MainActivity.java
+++ app/src/main/java/com/romanpulov/fingerlocker/MainActivity.java
@@ -1,14 +1,61 @@
 package com.romanpulov.fingerlocker;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.admin.DeviceAdminReceiver;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = "FingerLocker";
+
+    // Interaction with the DevicePolicyManager
+    DevicePolicyManager mDPM;
+    ComponentName mDeviceAdminSample;
+
+    private static final int REQUEST_CODE_ENABLE_ADMIN = 1;
+
+    /**
+     * Helper to determine if we are an active admin
+     */
+    private boolean isActiveAdmin() {
+        return mDPM.isAdminActive(mDeviceAdminSample);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Prepare to work with the DPM
+        mDPM = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+        mDeviceAdminSample = new ComponentName(this, DeviceAdminMainReceiver.class);
+
+        if (!isActiveAdmin()) {
+            // Launch the activity to have the user enable our admin.
+            Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mDeviceAdminSample);
+            intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,
+                    getString(R.string.add_admin_extra_app_text));
+            startActivityForResult(intent, REQUEST_CODE_ENABLE_ADMIN);
+            // return false - don't update checkbox until we're really active
+        } else {
+            //mDPM.removeActiveAdmin(mDeviceAdminSample);
+            mDPM.lockNow();
+        }
         finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "resultCode=" + resultCode);
+    }
+
+    public static class DeviceAdminMainReceiver extends DeviceAdminReceiver {
     }
 }
